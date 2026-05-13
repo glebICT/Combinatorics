bd97ef3cedc60217f8a308899f3de7a28bc9b01e
glebICT <147143396+glebICT@users.noreply.github.com>
2026-05-13 11:32:24 +0400

    Add example of permutations using itertools

---
+++ permutations_example.py
@@ -0,0 +1,13 @@
+# import the product function from itertools module
+from itertools import permutations
+
+print("All the permutations of the given list is:")
+print(list(permutations([1, 'geeks'], 2)))
+print()
+
+print("All the permutations of the given string is:")
+print(list(permutations('AB')))
+print()
+
+print("All the permutations of the given container is:")
+print(list(permutations(range(3), 2)))
